310d58eb5d7d90ded8f981cae65d607500ecaefa
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-28 18:32:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,14 @@ attr_accessor :total, :discount
     @cart = []
 end
 
-  def add_item(title, price)
+  def add_item(item, price, quantity = 1)
+    item_info = {}
+    item_info[:name] = item
+    item_info[:price] = price
+    item_info[:quantity] = quantity
+
+    @cart << item_info
+    @total += price * quantity
 
   end
 
