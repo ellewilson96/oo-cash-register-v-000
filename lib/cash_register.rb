83163d47207d5db3422d4151e9befd7a61a8bd41
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-28 18:11:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,12 +3,15 @@ class CashRegister
 attr_accessor :total
 
   def initialize(total)
+    @total = total
     @total = 0
   end
 
   def add_item(title, price)
     self.total += 1
   end
+
+  def discount
 
 
 
